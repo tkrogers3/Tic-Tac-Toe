5cc1d10f004091d6ecbabd7372281c06bf34a4b3
tkrogers3 <tkrogers3@gmail.com>
2020-03-09 13:08:01 -0400

    initialize files

---
+++ README.md
@@ -0,0 +1,8 @@
+Tic-Tac-Toe project using HTML, CSS, JS, Bootstrap.
+
+Must be:
+rendered dynamically
+have 9 blocks
+have a player turn display
+have a start/restart button
+have a player 1/2 winner notification
---
+++ script.js
@@ -0,0 +1,16 @@
+let container=document.createElement("div");
+container.id="container";
+container.className = "container";
+
+function createGame() {
+    let title = document.createElement("h1");
+    title.innerHTML="Tic-Tac-Toe";
+    title.id= "h1";
+    div1.appendChild(h1);
+
+}
+
+
+
+/* pseudo code create rows and columns 
+1 row with 3 columns  
